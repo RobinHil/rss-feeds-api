a3e5ecf61e72845ff27b140656951be79aa9c5a5
RobinH <hilairerob84@gmail.com>
2024-10-25 11:06:56 +0200

    Update packages

--- src/scripts/user.ts
+++ src/scripts/user.ts
@@ -1,25 +1,62 @@
 /**
+ * Interface représentant un utilisateur dans le système.
  * @interface User
- * @description Représente un utilisateur dans le système
+ * @typedef {Object} User
+ * @property {string} id - Identifiant unique de l'utilisateur
+ * @property {string} name - Nom de l'utilisateur
+ * @property {string} email - Email de l'utilisateur
  */
 interface User {
     id: string;
     name: string;
+    email: string;
 }
 
 /**
+ * Crée un nouvel utilisateur.
  * @function createUser
- * @description Crée un nouvel utilisateur
  * @param {string} name - Le nom de l'utilisateur
+ * @param {string} email - L'email de l'utilisateur
  * @returns {User} Un nouvel objet utilisateur
- * @throws {Error} Si le nom est vide
+ * @throws {Error} Si le nom ou l'email est vide
  */
-function createUser(name: string): User {
-    if (!name) {
-        throw new Error('Le nom ne peut pas être vide');
+function createUser(name: string, email: string): User {
+    if (!name || !email) {
+        throw new Error('Le nom et l\'email sont requis');
     }
     return {
         id: Math.random().toString(36).substr(2, 9),
-        name
+        name,
+        email
     };
 }
+
+/**
+ * Service de gestion des utilisateurs.
+ * @class UserService
+ */
+class UserService {
+    private users: User[] = [];
+
+    /**
+     * Ajoute un utilisateur à la liste.
+     * @method
+     * @param {User} user - L'utilisateur à ajouter
+     * @returns {void}
+     */
+    addUser(user: User): void {
+        this.users.push(user);
+    }
+
+    /**
+     * Récupère un utilisateur par son ID.
+     * @method
+     * @param {string} id - L'ID de l'utilisateur
+     * @returns {User|undefined} L'utilisateur trouvé ou undefined
+     */
+    getUserById(id: string): User | undefined {
+        return this.users.find(user => user.id === id);
+    }
+}
+
+export { User, createUser, UserService };
